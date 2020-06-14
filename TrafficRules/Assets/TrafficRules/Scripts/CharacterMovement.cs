86178384e9397fb9e6111d98a159f969e5de21b3
NhutKhanh <nhutkhanhng@gmail.com>
2020-06-14 21:55:41 +0700

    add some 3rd party, and some resource

---
+++ TrafficRules/Assets/TrafficRules/Scripts/CharacterMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KCharacterController
+{
+    public class CharacterMovement : MonoBehaviour
+    {
+        protected float m_MovingTurnSpeed = 360;
+        protected float m_StationaryTurnSpeed = 180;
+
+
+    }
+}
